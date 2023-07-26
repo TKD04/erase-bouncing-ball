7d4873e5bb3df1b168aa41b79b4ec35a9c804b8d
TKD04 <tkd04devpub@outlook.com>
2023-07-26 22:13:33 +0900

    Fix typos

--- src/ts/usecase/getRandomNumber.ts
+++ src/ts/usecase/getRandomNumber.ts
@@ -1,6 +1,6 @@
 export default (min: number, max: number): number => {
   if (min >= max) {
-    throw new RangeError("min must less than max.");
+    throw new RangeError("min must be less than max.");
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
