4eefcf534f624a20e893ff8763d7855a84145b9b
TKD04 <tkd04fordev@outlook.com>
2025-02-13 01:46:03 +0900

    Add Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,8 @@
+// eslint-disable-next-line jsdoc/check-tag-names
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   coverageProvider: "v8",
-  roots: ["<rootDir>/src"],
-  testEnvironment: "jsdom",
   preset: "ts-jest",
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
 };
