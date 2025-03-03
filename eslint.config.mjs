40886441beef19b534d2866691dad16f5c666e63
TKD04 <tkd04fordev@outlook.com>
2025-03-04 00:06:12 +0900

    Remove unused `**/*.test.{js,mjs,cjs}` from flie in jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -136,7 +136,7 @@ export default tseslint.config(
   },
   {
     extends: [jestPlugin.configs["flat/all"]],
-    files: ["**/*.test.{js,mjs,cjs,ts}"],
+    files: ["**/*.test.ts"],
     languageOptions: {
       globals: globals.jest,
     },
