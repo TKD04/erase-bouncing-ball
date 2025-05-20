1aa869d85f506822ea2bca27c6e2b05fe243cb61
TKD04 <tkd04fordev@outlook.com>
2025-05-20 11:49:05 +0900

    Add `@see` to vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
+/**
+ * @see https://vite.dev/config/
+ */
 export default defineConfig({
   base: "https://tkd04.github.io/erase-bouncing-ball/",
   build: {
